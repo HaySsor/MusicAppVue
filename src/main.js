8556c17245c1a97f5f19cf77becf1d43d25c164e
Przemek <93125773+HaySsor@users.noreply.github.com>
2022-11-25 14:53:54 +0100

    add progress bard to load page , with NProgress, change to dynamic import route

--- src/main.js
+++ src/main.js
@@ -8,12 +8,16 @@ import { auth } from "./includes/firebase"
 import Icon from '../src/directives/icon'
 import i18n from './includes/i18n'
 import { registerSW } from 'virtual:pwa-register'
+import GlobalComponents from './includes/_globals'
+import progressBar from './includes/progressBar'
 
 
 import "./assets/main.css";
 import "./assets/basic.css";
+import 'nprogress/nprogress.css'
 
 registerSW({ immediate: true })
+progressBar(router)
 
 let app
 
@@ -27,6 +31,7 @@ auth.onAuthStateChanged(() => {
         app.use(router);
         app.use(VeeValidatePlugin)
         app.use(i18n)
+        app.use(GlobalComponents)
         app.directive("icon", Icon)
 
 
--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -141,21 +141,24 @@ export default {
       });
     },
   },
-  async created() {
-    const docRef = await doc(songsCollection, this.$route.params.id);
+  async beforeRouteEnter(to, from, next) {
+    const docRef = await doc(songsCollection, to.params.id);
     const docSnapshot = await getDoc(docRef);
 
-    if (!docSnapshot.exists()) {
-      this.$router.push({name: 'home'});
+    next((vm)=>{
+      if (!docSnapshot.exists()) {
+      vm.$router.push({name: 'home'});
       return;
     }
 
-    const {sort} = this.$route.query;
+    const {sort} = vm.$route.query;
 
-    this.sort = sort === '1' || sort === '2' ? sort : '1';
+    vm.sort = sort === '1' || sort === '2' ? sort : '1';
 
-    this.song = docSnapshot.data();
-    this.getComments();
+    vm.song = docSnapshot.data();
+    vm.getComments();
+    })
+    
   },
   methods: {
     ...mapActions(usePlayerStore, ['newSong']),
